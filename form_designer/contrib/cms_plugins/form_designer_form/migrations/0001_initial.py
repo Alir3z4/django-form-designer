1cb04a51d0c855873828d89ddedb49a5bf373219
Alireza Savand <alireza.savand@gmail.com>
2013-07-26 23:17:17 +0400

    Add migrations for form_designer_form which is actual cmsplugin itself

---
+++ form_designer/contrib/cms_plugins/form_designer_form/migrations/0001_initial.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'CMSFormDefinition'
+        db.create_table(u'cmsplugin_cmsformdefinition', (
+            (u'cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
+            ('form_definition', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['form_designer.FormDefinition'])),
+        ))
+        db.send_create_signal(u'form_designer_form', ['CMSFormDefinition'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'CMSFormDefinition'
+        db.delete_table(u'cmsplugin_cmsformdefinition')
+
+
+    models = {
+        'cms.cmsplugin': {
+            'Meta': {'object_name': 'CMSPlugin'},
+            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
+            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
+            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
+        },
+        u'form_designer.formdefinition': {
+            'Meta': {'object_name': 'FormDefinition'},
+            'action': ('django.db.models.fields.URLField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'allow_get_initial': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'body': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'display_logged': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'error_message': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'form_template_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'log_data': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'mail_from': ('form_designer.fields.TemplateCharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'mail_subject': ('form_designer.fields.TemplateCharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'mail_to': ('form_designer.fields.TemplateCharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'mail_uploaded_files': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'message_template': ('form_designer.fields.TemplateTextField', [], {'null': 'True', 'blank': 'True'}),
+            'method': ('django.db.models.fields.CharField', [], {'default': "'POST'", 'max_length': '10'}),
+            'name': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255'}),
+            'private_hash': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '40'}),
+            'public_hash': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '40'}),
+            'require_hash': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'save_uploaded_files': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'submit_label': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'success_clear': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'success_message': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'success_redirect': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        u'form_designer_form.cmsformdefinition': {
+            'Meta': {'object_name': 'CMSFormDefinition', 'db_table': "u'cmsplugin_cmsformdefinition'", '_ormbases': ['cms.CMSPlugin']},
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
+            'form_definition': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['form_designer.FormDefinition']"})
+        }
+    }
+
+    complete_apps = ['form_designer_form']
